9c1a237272bf76e28be908895876249ed6040a7f
MakhehlaMkhize <mkh22001@byui.edu>
2023-01-09 10:25:16 +0200

    Prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+        Console.Write("What is your grade percentage?");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        if (gradePercentage >= 90 )
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradePercentage < 60)
+        {
+            letter = "F";
+        }
+        Console.Write($"Your grade is '{letter}' ");
+        if ( gradePercentage >= 70)
+        {
+            Console.Write("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.Write("Under achieved. You can do better next time.");
+        } 
     }
 }
