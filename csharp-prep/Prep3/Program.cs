ffd2b088031992a8230e76f831ce6b87fd05c04a
MakhehlaMkhize <mkh22001@byui.edu>
2023-01-10 21:33:42 +0200

    week 2 C# practice

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1,101);
+        
+        int userGuess = 0;
+        while (userGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+            
+            if(userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if(userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List <int> myNumbers = new List<int>();
+
+        Console.WriteLine(" Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+
+        int sumOfNumbers = 0;
+
+        int largestNumber = 0;
+
+        while(number != 0)
+        {
+            Console.Write("Enter a number: ");
+
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                myNumbers.Add(number);
+            }
+            
+            if (number > largestNumber)
+            {
+                largestNumber = number;
+            }
+
+            sumOfNumbers = sumOfNumbers + number;
+        }
+        double aveOfNumbers = (double)sumOfNumbers/myNumbers.Count;
+
+        Console.WriteLine($"The sum is: {sumOfNumbers}");
+
+        Console.WriteLine($"The average is: {aveOfNumbers}");
+
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
     }
 }
